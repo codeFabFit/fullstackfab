619041902179ed6764aab7c6fca173adbd8fe062
codeFabFit <fabgar14@gmail.com>
2023-12-02 19:44:16 -0500

    customized CSS finished JS

--- script.js
+++ script.js
@@ -36,22 +36,90 @@ function headerShadow (){
 
 /* ##### Typing Effect ##### */
 
-
+var typingEffect = new Typed(".typedText",{
+    strings : ["Designer","Youtuber","Developer", "Trainer"],
+    loop : true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay: 2000
+})
 
 
 /* ##### Scroll reveal animation ##### */
 
-
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "80px",
+    duration: 2000,
+    reset: true
+})
 
 
 /* ##### Home ##### */
 
+sr.reveal('.featured-text-card', {})
+sr.reveal('.featured-name', {delay: 100})
+sr.reveal('.featured-text-info', {delay: 200})
+sr.reveal('.featured-text-btn', {delay: 200})
+sr.reveal('.social_icons', {delay: 200})
+sr.reveal('.featured-image', {delay: 300})
+
+/* ##### Project box ##### */
+
+sr.reveal('.project-box', {interval: 200})
+
+/* ##### Headings ##### */
 
 
+sr.reveal('.top-header', {})
 
-/* ##### Project section ##### */
+/* ##### About INFO ##### */
+
+const srLeft = ScrollReveal ({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srLeft.reveal('.about-info', {delay: 100})
+srLeft.reveal('.contact-info', {delay: 100})
 
 
+/* ##### About Skills ##### */
+
+const srRight = ScrollReveal ({
+    origin: 'right',
+    distance: '800px',
+    duration: 2000,
+    reset: true
+})
 
 
-/* ##### Featured box ##### */
+/* ##### Change active link ##### */
+
+const sections  = document.querySelectorAll("section[id]")
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach(current => {
+        const sectionHeight = current.offSetHeight, 
+            sectionTop = current.offserTop - 50,
+            sectionId = current.getAttribute('id')
+
+            if(scroolY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('actve-link');{
+
+                }
+            } else {
+
+                document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('actve-link');{
+                    
+                }
+            }
+        })
+
+}
+
+window.addEventListener('scroll', scrollActive)
